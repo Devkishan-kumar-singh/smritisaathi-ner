e6f5046e1817f07ebef764f77a4d3947237751d2
Dev <kishan394069@gmail.com>
2026-09-19 13:39:09 +0530

    Add new cognitive games and expand caregiver analytics

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='smriti-v16-instant-sync';
+const CACHE='smriti-v17-expanded-games';
 const CORE=['/','/styles.css','/auth-v4.css','/mobile-v5.css','/manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>clients.claim())));
